a0a716988770c2675969de82a94be14d8eb3b5f0
Scott Taylor <scott.c.taylor@mac.com>
2017-08-15 03:25:34 -0400

    fix server cache

--- src/relay/environment.js
+++ src/relay/environment.js
@@ -1,12 +1,12 @@
 import { Environment, Network, RecordSource, Store } from 'relay-runtime';
 import createFetch from 'relay/fetcher';
 
-function createEnviroment(url) {
+function createEnviroment(url, requestCache = {}) {
   const recordSource = new RecordSource();
   const store = new Store(recordSource);
 
   const environment = new Environment({
-    network: Network.create(createFetch(url)),
+    network: Network.create(createFetch(url, requestCache)),
     store,
   });
 
